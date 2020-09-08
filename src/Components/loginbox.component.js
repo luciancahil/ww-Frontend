fa6bf46d3d67ea33060d1ad1e4ff6f14b3d61dd4
Roy He <royhe62@yahoo.ca>
2020-09-07 22:32:23 -0700

    Added Login Styling

--- src/Components/loginbox.component.js
+++ src/Components/loginbox.component.js
@@ -1,15 +1,29 @@
 import React from 'react';
-
+//tortilla
 class Login extends React.Component {
   render() {
     return(
         <div id = "LoginBoxOuter">
             <div id = "Loginbox">
-                <h3>Login</h3>
-                <plaintext>Username:</plaintext><input type = "text"></input>
-                <plaintext>Password:</plaintext><input type = "text"></input><br></br>
-                <button>Sign In</button>
-                <button>Sign Up</button>
+                <div id = "LoginTop"><h2>Login</h2></div>
+
+                <div id = "LoginBottom">
+                    <div id = "LoginForms">
+                        <plaintext>Username:</plaintext><input type = "text"></input><br></br>
+                        <plaintext>Password:</plaintext><input type = "text"></input><br></br>
+                    </div>
+
+                    <div className = "LoginButtonOuter">
+                        <div className = "LoginButtonInner">
+                            <button>Sign in</button>
+                        </div>
+                    </div>
+                    <div className = "LoginButtonOuter">
+                        <div className = "LoginButtonInner">
+                            <button>Sign up</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
