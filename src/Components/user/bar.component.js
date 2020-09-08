ecd08cd778f5568cd64292a4e4c6ed918199ff3f
Roy He <royhe62@yahoo.ca>
2020-09-08 14:48:21 -0700

    Styled Bars

--- src/Components/user/bar.component.js
+++ src/Components/user/bar.component.js
@@ -3,30 +3,31 @@ import React from 'react';
 class Bar extends React.Component {
   render() {
     return (
-        <div id = "barOuter">
-            <span>
-                <h2>Date</h2>
-            </span>
-            <span>
-                <h2>Height</h2>
-            </span>
-            <span>
-                <h2>Abdomen</h2>
-            </span>
-            <span>
-                <h2>Neck</h2>
-            </span>
-            <span>
-                <h2>Body Fat %</h2>
-            </span>
-            <span>
-                <h2>Daily Change</h2>
-            </span>
-            <span>
-                <h2>7-Day Change</h2>
-            </span>
+        <div id = "bars">
+            <div id = "bar">
+                <span>
+                    <h2>Date</h2>
+                </span>
+                <span>
+                    <h2>Height</h2>
+                </span>
+                <span>
+                    <h2>Abdomen</h2>
+                </span>
+                <span>
+                    <h2>Neck</h2>
+                </span>
+                <span>
+                    <h2>Body Fat %</h2>
+                </span>
+                <span>
+                    <h2>Daily Change</h2>
+                </span>
+                <span>
+                    <h2>7-Day Change</h2>
+                </span>
+            </div>
         </div>
-
     )
   }
 }
