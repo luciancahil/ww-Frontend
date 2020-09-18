b3947cc7d1bcca0b999434d032508656186d1d0b
Roy He <royhe62@yahoo.ca>
2020-09-17 17:10:05 -0700

    Imported login box into signup

--- src/Components/signup.component.js
+++ src/Components/signup.component.js
@@ -1,8 +1,51 @@
 import React from 'react';
+import LoginError from './loginerror.component'
+
 
 class SignUp extends React.Component {
+  constructor(props){
+    super(props);
+
+    //this.onSubmit = this.onSubmit.bind(this);
+    //this.onChangeUsername = this.onChangeUsername.bind(this);
+    //this.onChangePassword = this.onChangePassword.bind(this);
+    this.state = {
+        signup_username: "" ,
+        signup_password: "",
+        signup_status: "Hello"
+    };
+    if(sessionStorage.getItem(this.props.randomSession + "username") !== null){
+        props.quickStart(sessionStorage.getItem(this.props.randomSession + "username"))
+    }
+  }
+
   render() {
-    return <h2>Signup Page</h2>;
+    return(
+        <div class = "BoxBoxOuter">
+            <div class = "Box">
+                <div class = "BoxTop"><h2>Login</h2></div>
+
+                <div class = "BoxBottom">
+                    <div class = "BoxForms">
+                        <plaintext>Username:</plaintext><input type = "text" value = {this.state.signup_username} onChange = {this.onChangeUsername}></input><br></br>
+                        <plaintext>Password:</plaintext><input type = "password" value = {this.state.signup_password} onChange = {this.onChangePassword}></input><br></br>
+                        <LoginError type = {this.state.signup_status}/>
+                    </div>
+
+                    <div className = "LoginButtonOuter">
+                        <div className = "LoginButtonInner">
+                            <button onClick = {this.onSubmit}>Sign in</button>
+                        </div>
+                    </div>
+                    <div className = "LoginButtonOuter">
+                        <div className = "LoginButtonInner">
+                            <a href = "/signup"><button>Sign up</button></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
   }
 }
 
