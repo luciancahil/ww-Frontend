dcc6f1a0ecaef9019ee1a222554d7da9b535c78a
Roy He <royhe62@yahoo.ca>
2020-09-20 21:25:41 -0700

    Added typing ability and second password box

--- src/Components/signup.component.js
+++ src/Components/signup.component.js
@@ -7,16 +7,36 @@ class SignUp extends React.Component {
     super(props);
 
     //this.onSubmit = this.onSubmit.bind(this);
-    //this.onChangeUsername = this.onChangeUsername.bind(this);
-    //this.onChangePassword = this.onChangePassword.bind(this);
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangePasswordOne = this.onChangePasswordOne.bind(this);
+    this.onChangePasswordTwo = this.onChangePasswordTwo.bind(this);
     this.state = {
         signup_username: "" ,
-        signup_password: "",
+        signup_password_one: "",
+        signup_password_two: "",
         signup_status: "Hello"
     };
     if(sessionStorage.getItem(this.props.randomSession + "username") !== null){
         props.quickStart(sessionStorage.getItem(this.props.randomSession + "username"))
     }
+  }
+
+  onChangeUsername(e){
+      this.setState({
+        signup_username : e.target.value
+      })
+  }
+
+  onChangePasswordOne(e){
+    this.setState({
+        signup_password_one : e.target.value
+    })
+  }
+
+  onChangePasswordTwo(e){
+    this.setState({
+        signup_password_two : e.target.value
+    })
   }
 
   render() {
@@ -28,7 +48,8 @@ class SignUp extends React.Component {
                 <div class = "BoxBottom">
                     <div class = "BoxForms">
                         <plaintext>Username:</plaintext><input type = "text" value = {this.state.signup_username} onChange = {this.onChangeUsername}></input><br></br>
-                        <plaintext>Password:</plaintext><input type = "password" value = {this.state.signup_password} onChange = {this.onChangePassword}></input><br></br>
+                        <plaintext>Password:</plaintext><input type = "password" value = {this.state.signup_password_one} onChange = {this.onChangePasswordOne}></input><br></br>
+                        <plaintext>Retype Password:</plaintext><input type = "password" value = {this.state.signup_password_two} onChange = {this.onChangePasswordTwo}></input><br></br>
                         <LoginError type = {this.state.signup_status}/>
                     </div>
 
